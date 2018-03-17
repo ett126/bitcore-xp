0d60c11137cc43c4481d3fdeebd0077d42d14fd1
ett126 <root@xp-insight-dev.cs928idcfcloud.internal>
2018-03-17 23:16:48 +0900

    Change to XP testnet Settings

--- networks.js
+++ networks.js
@@ -31,25 +31,23 @@ exports.mainnet = exports.livenet;
 
 exports.testnet = {
   name: 'testnet',
-  magic: hex('0b110907'),
-  addressVersion: 0x6f,
+  magic: hex('c0efcdf2'),
+  addressVersion: 111,
   privKeyVersion: 239,
   P2SHVersion: 196,
   hkeyPublicVersion: 0x043587cf,
   hkeyPrivateVersion: 0x04358394,
   genesisBlock: {
-    hash: hex('43497FD7F826957108F4A30FD9CEC3AEBA79972084E90EAD01EA330900000000'),
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+    hash: hex('0000E097477B585E7289F4C23C4FF639A8775E8F4D2C715586F160FFF8869278'),
+    merkle_root: hex('CC83C2272FA6B9D6FE6C322C9730BCAE47278195A2D64A9D2F796BFB41EEC001'),
     height: 0,
-    nonce: 414098458,
+    nonce: 175602,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1296688602,
-    bits: 486604799,
+    timestamp: 1472006328,
+    bits: 4245812455,
   },
   dnsSeeds: [
-    'testnet-seed.bitcoin.petertodd.org',
-    'testnet-seed.bluematt.me'
   ],
-  defaultClientPort: 18333
+  defaultClientPort: 17778
 };
